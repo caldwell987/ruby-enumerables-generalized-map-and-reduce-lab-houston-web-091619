aaf4ff53449a036a37b60738e8ce55fac87bbb1b
David Caldwell <david@avvitt.com>
2019-09-15 05:31:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,4 +2,23 @@ def map(array)
   new =[]
   i = 0
   while i< array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
 
+
+def reduce(array, SV=nill)
+  if SV 
+    sum = SV
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array(sum, array[i])
+  i += 1
+end
+sum
+end
