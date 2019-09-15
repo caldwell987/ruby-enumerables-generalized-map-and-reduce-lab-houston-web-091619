7971abcfac784242f071a3cb479f61119ae29d29
David Caldwell <david@avvitt.com>
2019-09-15 05:27:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,15 +1,5 @@
-map(numbers)
-  numbers.map {|n| n * -1}
-
-
-
-def map(dune)  
-  dune.map {|n| n}
-end
-
-def third(numbers)
- numbers_two.map {|n| n * 2}
-end
-
-
+def map(array)
+  new =[]
+  i = 0
+  while i< array.length
 
